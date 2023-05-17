e4ad9cb97c4e00f7df6f957f1c555a2b5c4d960b
Bernardo Borges <bernardobbm334@gmail.com>
2023-05-17 19:11:17 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const app = fastify();
+const prisma = new PrismaClient();
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333');
+  });
