62a8f4b31f8517dbf3d2c196b04993f6cc468112
Bernardo Borges <bernardobbm334@gmail.com>
2023-06-01 00:30:54 -0300

    added logout button

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,11 +1,47 @@
-import { Text, View } from "react-native";
+import { Link, useRouter } from 'expo-router';
+import { TouchableOpacity, ScrollView, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Icon from '@expo/vector-icons/Feather';
+import * as SecureStore from 'expo-secure-store';
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg';
 
 export default function Memories() {
+  const { bottom, top } = useSafeAreaInsets();
+  const router = useRouter();
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token');
+
+    router.push('/');
+  }
+
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="">
-        Memories
-      </Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{
+        paddingBottom: bottom + 10,
+        paddingTop: top,
+      }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+
+        <View className="flex-row gap-2 ">
+          <TouchableOpacity
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+            onPress={signOut}
+          >
+            <Icon name="log-out" size={16} color={'#000'} />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color={'#000'} />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
