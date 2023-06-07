b6e75f2d36ccac714f09239ad5bb009ee75daaa8
Bernardo Borges <bernardobbm334@gmail.com>
2023-06-07 17:56:39 -0300

    added fetch memories in api and layout for listing

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: [
+      'avatars.githubusercontent.com',
+      '127.0.0.1',
+      '192.168.1.9',
+      '192.168.1.11',
+    ],
   },
 };
 
