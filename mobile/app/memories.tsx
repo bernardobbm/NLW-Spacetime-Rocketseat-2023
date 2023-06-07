ed0116b765a8b883c166fa960ff55fa72088c59c
Bernardo Borges <bernardobbm334@gmail.com>
2023-06-07 15:12:50 -0300

    added fetch memories in api and layout for listing

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,14 +1,29 @@
-import { Link, useRouter } from 'expo-router';
-import { TouchableOpacity, ScrollView, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { TouchableOpacity, ScrollView, View, Text, Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import Icon from '@expo/vector-icons/Feather';
+import { Link, useRouter } from 'expo-router';
+import { ResizeMode, Video } from 'expo-av';
 import * as SecureStore from 'expo-secure-store';
+import Icon from '@expo/vector-icons/Feather';
+import dayjs from 'dayjs';
+import ptBr from 'dayjs/locale/pt-br';
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg';
+import { api } from '../src/lib/api';
+
+dayjs.locale(ptBr);
+
+interface Memory {
+  coverUrl: string;
+  excerpt: string;
+  createdAt: string;
+  id: string;
+}
 
 export default function Memories() {
   const { bottom, top } = useSafeAreaInsets();
   const router = useRouter();
+  const [memories, setMemories] = useState<Memory[]>([]);
 
   async function signOut() {
     await SecureStore.deleteItemAsync('token');
@@ -16,18 +31,34 @@ export default function Memories() {
     router.push('/');
   }
 
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token');
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    setMemories(response.data);
+  }
+
+  useEffect(() => {
+    loadMemories();
+  }, []);
+
   return (
     <ScrollView
-      className="flex-1 px-8"
+      className="flex-1"
       contentContainerStyle={{
         paddingBottom: bottom + 10,
         paddingTop: top,
       }}
     >
-      <View className="mt-4 flex-row items-center justify-between">
+      <View className="mt-4 flex-row items-center justify-between px-8">
         <NLWLogo />
 
-        <View className="flex-row gap-2 ">
+        <View className="flex-row gap-2">
           <TouchableOpacity
             className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
             onPress={signOut}
@@ -41,6 +72,55 @@ export default function Memories() {
             </TouchableOpacity>
           </Link>
         </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        {memories.map((memory) => {
+          const coverType = memory.coverUrl.slice(-3);
+
+          return (
+            <View key={memory.id} className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className="font-body text-xs text-gray-100">
+                  {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+
+              <View className="space-y-4 px-8">
+                {coverType === 'jpg' || coverType === 'png' ? (
+                  <Image
+                    source={{
+                      uri: memory.coverUrl,
+                    }}
+                    className="aspect-video w-full rounded-lg"
+                    alt=""
+                  />
+                ) : (
+                  <Video
+                    source={{ uri: memory.coverUrl }}
+                    useNativeControls={false}
+                    resizeMode={ResizeMode.COVER}
+                    className="aspect-video w-full rounded-lg"
+                  />
+                )}
+
+                <Text className="font-body text-base leading-relaxed text-gray-200">
+                  {memory.excerpt}
+                </Text>
+
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler mais
+                    </Text>
+                    <Icon name="arrow-right" size={16} color={'#9e9ea0'}></Icon>
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          );
+        })}
       </View>
     </ScrollView>
   );
